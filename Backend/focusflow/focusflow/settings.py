6fd2172c90a81a3b2e5caecc92b62287bc6fcaa3
Szilveszter <nmszilveszter@gmail.com>
2025-06-24 16:38:37 +0200

    Auto generated models

--- Backend/focusflow/focusflow/settings.py
+++ Backend/focusflow/focusflow/settings.py
@@ -73,9 +73,16 @@ WSGI_APPLICATION = "focusflow.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'focusflow',         
+        'USER': 'root',          
+        'PASSWORD': 'SajtosKifli123',              
+        'HOST': 'localhost',               
+        'PORT': '3306',                    
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
