4667e85dd1a2efbbe96a0b25c97363a92c21aa31
Szilveszter <nmszilveszter@gmail.com>
2025-06-30 12:12:59 +0200

    Add full Todos endpoint with filtering and search

--- Backend/focusflow/todo/urls.py
+++ Backend/focusflow/todo/urls.py
@@ -4,7 +4,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.routers import DefaultRouter
+from .views import CategoriesViewSet,TodosViewSet
 
+router = DefaultRouter()
+router.register(r'categories', CategoriesViewSet, basename='categories')
+router.register(r'todos', TodosViewSet, basename='todos')
 
 
 urlpatterns = [
@@ -13,4 +18,5 @@ urlpatterns = [
     #JWT Token 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Login, token request
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Token refresh
+    path('', include(router.urls)), 
 ]
